d46c6d7b6dbfaf044a0d83234ab7afac7175959f
Roman Tverdokhleb <tverpokhdeb@icloud.com>
2025-06-11 14:25:12 +0300

    added new composer app icon

---
+++ LiquidQuiz/Modules/CreateQuizView.swift
@@ -0,0 +1,18 @@
+//
+//  CreateQuizView.swift
+//  LiquidQuiz
+//
+//  Created by Roman Tverdokhleb on 11/06/2025.
+//
+
+import SwiftUI
+
+struct CreateQuizView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CreateQuizView()
+}
---
+++ LiquidQuiz/Modules/CreateQuizView2.swift
@@ -0,0 +1,18 @@
+//
+//  CreateQuizView2.swift
+//  LiquidQuiz
+//
+//  Created by Roman Tverdokhleb on 11/06/2025.
+//
+
+import SwiftUI
+
+struct CreateQuizView2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CreateQuizView2()
+}
