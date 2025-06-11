b8b368815b3dc149ac54f8545753d0d2df14e585
Roman Tverdokhleb <tverpokhdeb@icloud.com>
2025-06-11 12:22:31 +0300

    Initial Commit

---
+++ LiquidQuiz/LiquidQuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiquidQuizApp.swift
+//  LiquidQuiz
+//
+//  Created by Roman Tverdokhleb on 11/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct LiquidQuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
